b75e2ccb63fad7ed93593d9083255a604ce159ef
fengyuwuyu <13120174340@139.com>
2019-01-02 23:28:36 +0800

    添加sql format

--- card-data/card-data-consume/src/main/java/com/bdtd/card/data/consume/DataConsumeApplication.java
+++ card-data/card-data-consume/src/main/java/com/bdtd/card/data/consume/DataConsumeApplication.java
@@ -3,13 +3,23 @@ package com.bdtd.card.data.consume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.bdtd.card.data.consume.dao.UserMapper;
+import com.bdtd.card.data.consume.model.User;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class DataConsumeApplication {
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(DataConsumeApplication.class, args);
+		ApplicationContext ac = SpringApplication.run(DataConsumeApplication.class, args);
+		UserMapper userMapper = ac.getBean(UserMapper.class);
+		System.out.println(userMapper.getByAccount("admin"));
+		Wrapper<User> wrapper = new QueryWrapper<>();
+		System.out.println(userMapper.selectList(wrapper ));
 	}
 
 }

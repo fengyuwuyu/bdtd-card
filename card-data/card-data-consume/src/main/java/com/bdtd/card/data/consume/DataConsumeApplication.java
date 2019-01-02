21fb3fb334a632743dd8e0b94a9d734aa9a49fd5
fengyuwuyu <13120174340@139.com>
2019-01-02 21:02:37 +0800

    完成card-data-consume初始化，添加p6spy格式化sql log输出

--- card-data/card-data-consume/src/main/java/com/bdtd/card/data/consume/DataConsumeApplication.java
+++ card-data/card-data-consume/src/main/java/com/bdtd/card/data/consume/DataConsumeApplication.java
@@ -3,23 +3,13 @@ package com.bdtd.card.data.consume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.bdtd.card.data.consume.dao.UserMapper;
-import com.bdtd.card.data.consume.model.User;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class DataConsumeApplication {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ac = SpringApplication.run(DataConsumeApplication.class, args);
-		UserMapper userMapper = ac.getBean(UserMapper.class);
-		System.out.println(userMapper.getByAccount("admin"));
-		Wrapper<User> wrapper = new QueryWrapper<>();
-		System.out.println(userMapper.selectList(wrapper ));
+		SpringApplication.run(DataConsumeApplication.class, args);
 	}
 
 }
--- card-data/card-data-consume/src/main/java/com/bdtd/card/data/consume/config/MybatisPlusConfig.java
+++ card-data/card-data-consume/src/main/java/com/bdtd/card/data/consume/config/MybatisPlusConfig.java
@@ -1,70 +1,32 @@
 package com.bdtd.card.data.consume.config;
 
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.bdtd.card.data.consume.config.properties.DbProperties;
 
 @Configuration
-@EnableTransactionManagement(order = 2)//由于引入多数据源，所以让spring事务的aop要在多数据源切换aop的后面
-@MapperScan(basePackages = {"com.bdtd.card.data.*.dao"})
+@EnableTransactionManagement //(order = 2)由于引入多数据源，所以让spring事务的aop要在多数据源切换aop的后面
+@MapperScan(basePackages = {"com.bdtd.card.data.consume.dao"})
 public class MybatisPlusConfig {
-	
-	@Autowired
-	private DbProperties dbProperties;
 
-    /**
-     * 另一个数据源
-     */
-	@Bean(name = "dataSource")
-//    @Primary
-    public DataSource bizDataSource() {
-		DruidDataSource dataSource = new DruidDataSource();
-		dbProperties.config(dataSource);
-        return dataSource;
-    }
-
-    @Bean(name = "sqlSessionFactory")
-//    @Primary
-    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
-        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/*/*Mapper.xml"));
-        return bean.getObject();
-    }
-
-    @Bean(name = "transactionManager")
-//    @Primary
-    public DataSourceTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
 
     @Bean(name = "sqlSessionTemplate")
 //    @Primary
     public SqlSessionTemplate sqlSessionTemplate(@Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
-    
-    @Bean
-    public ISqlInjector LogicSqlInjector() {
-    	return new LogicSqlInjector();
-    }
+
+//    @Bean
+//    public ISqlInjector LogicSqlInjector() {
+//    	return new LogicSqlInjector();
+//    }
 
     /**
      * mybatis-plus分页插件

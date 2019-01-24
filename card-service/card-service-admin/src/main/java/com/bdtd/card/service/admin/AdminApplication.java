d76d01576e9824ae7232f4a547b9b7e9baf3bf45
lilei <13120174340@139.com>
2019-01-24 17:28:56 +0800

    add template files

--- card-service/card-service-admin/src/main/java/com/bdtd/card/service/admin/AdminApplication.java
+++ card-service/card-service-admin/src/main/java/com/bdtd/card/service/admin/AdminApplication.java
@@ -4,13 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.bdtd.card.data.admin.dao.UserMapper;
-import com.bdtd.card.data.admin.model.User;
 
 @SpringBootApplication(scanBasePackages= {"com.bdtd.card"})
 @EnableTransactionManagement
@@ -19,11 +13,7 @@ import com.bdtd.card.data.admin.model.User;
 public class AdminApplication {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ac = SpringApplication.run(AdminApplication.class, args);
-		UserMapper userMapper = ac.getBean(UserMapper.class);
-		QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-		queryWrapper.eq("account", "admin");
-		userMapper.selectOne(queryWrapper);
+		SpringApplication.run(AdminApplication.class, args);
 	}
 
 }
